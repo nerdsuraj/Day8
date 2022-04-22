e8db6208e128fd8d1303d5730d369b5d356a57b3
suraj <soorajkrpandit@gmail.com>
2022-04-22 16:46:44 +0530

    Add project files.

---
+++ Day8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpPresent.EmpolyeeWage();
+            Console.ReadLine();
+        }
+    }
+}
